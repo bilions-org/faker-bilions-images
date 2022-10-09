49b707042857f7e496c9e1c95c41b70de8c6bbee
AJ <necessarylion@gmail.com>
2022-10-09 13:48:27 +0700

    added support for image text

--- src/FakerImageProvider.php
+++ src/FakerImageProvider.php
@@ -13,18 +13,32 @@ class FakerImageProvider extends Base
         int $height = 480,
         string $imageExtension = 'png'
     ) {
+        
         list($r, $g, $b) = self::getBackgroundColor();
         $image = imagecreate($width, $height);
         $background = imagecolorallocate($image, $r, $g, $b);
         $textColor = imagecolorallocate($image, 255, 255, 255);
+
+        // set text and center text
+        $text = self::getText();
+        $font = realpath(dirname(__FILE__).'/ReadexPro-Regular.ttf');
+        $size = 30;
+        $box = imagettfbbox($size, 0, $font, $text);
+        $textWidth = abs($box[2]) - abs($box[0]);
+        $textHeight = abs($box[5]) - abs($box[3]);
+        $x = round(($width - $textWidth) / 2, 0);
+        $y = round(($height + $textHeight) / 2, 0);
+        imagettftext($image, $size, 0, $x, $y, $textColor, $font, $text);
+
         imagecolordeallocate($image, $textColor);
         imagecolordeallocate($image, $background);
 
         $dir = $dir === null ? '/tmp' : $dir;
-        $name = md5(uniqid(empty($_SERVER['SERVER_ADDR']) ? '' : $_SERVER['SERVER_ADDR'], true));
+        $name = md5(uniqid().time().random_int(10000, 99999));
         $filename = $name . "." . $imageExtension;
         $filePath = $dir . DIRECTORY_SEPARATOR . $filename;
         imagejpeg($image, $filePath);
+        
         return $filePath;
     }
 
